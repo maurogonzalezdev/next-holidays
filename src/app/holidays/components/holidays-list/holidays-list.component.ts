8b0213cae64a700f7fe0939963517b1bdcc3fd59
Mauricio Gonzalez <hello@mauro-gonzalez.dev>
2024-10-24 19:11:42 -0300

    added relative country name

--- src/app/holidays/components/holidays-list/holidays-list.component.ts
+++ src/app/holidays/components/holidays-list/holidays-list.component.ts
@@ -1,26 +1,42 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IHoliday } from '../../interfaces/holiday.interface';
+import { ICountry } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-holidays-list',
   templateUrl: './holidays-list.component.html',
-  styles: ``,
 })
 export class HolidaysListComponent {
   private _holidays: IHoliday[] = [];
-
-  public loadedHolidays: boolean = false;
+  private country?: ICountry;
+  private _loadedHolidays: boolean = false;
 
   @Input()
   set setHolidays(holidays: IHoliday[]) {
     if (holidays.length > 0) {
       this._holidays = holidays;
-      this.loadedHolidays = !this.loadedHolidays;
+      this._loadedHolidays = !this._loadedHolidays;
+    }
+    return;
+  }
+
+  @Input()
+  set setCountry(country: ICountry) {
+    if (country) {
+      this.country = country;
     }
     return;
   }
 
   get getHolidays(): IHoliday[] {
     return this._holidays;
+  }
+
+  get getCountry(): ICountry {
+    return this.country!;
+  }
+
+  get getLoadedHolidays(): boolean {
+    return this._loadedHolidays;
   }
 }
--- src/app/shared/pages/main-page/main-page.component.ts
+++ src/app/shared/pages/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ILocation } from '../../../location/interfaces/location.interface';
 import { HolidaysService } from '../../../holidays/services/holidays.service';
 import { IHoliday } from '../../../holidays/interfaces/holiday.interface';
+import { ICountry } from '../../../holidays/interfaces/country.interface';
 
 @Component({
   selector: 'app-shared-main-page',
@@ -9,11 +10,24 @@ import { IHoliday } from '../../../holidays/interfaces/holiday.interface';
 })
 export class MainPageComponent {
   public holidays: IHoliday[] = [];
+  public country: ICountry = {
+    country: '',
+    flag: '',
+  };
 
   constructor(private holidaysService: HolidaysService) {}
 
-  public getLocation({ iso2: countryCode }: ILocation): void {
+  public getLocation({
+    iso2: countryCode,
+    countryNativeName,
+    countryFlag,
+  }: ILocation): void {
     let year: number = new Date().getFullYear();
+
+    this.country = {
+      country: countryNativeName,
+      flag: countryFlag,
+    };
 
     this.holidaysService
       .getHolidays(year, countryCode)
